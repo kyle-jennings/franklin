590876accfb301fd54de63ccf061dd3f1af31e65
kyle-jennings <kjennings@gmail.com>
2018-07-07 13:05:17 -0400

    finished script

--- svnsync.sh
+++ svnsync.sh
@@ -1,6 +1,34 @@
-#!/bin/bash
-echo svn co https:plugins.svn/wordpress.org/franklin svn
+#!/bin/sh
+
+message='';
+tag="$(git tag | head -n 1)";
+commit="$(git rev-parse --short HEAD)";
+
+if [ -z "$1" ]; then
+        if [ ! -z $tag ]; then 
+                message=$tag;
+            else
+                message=$commit;
+        fi
+    else
+        message=$1;
+fi
+
+
+if [ ! -d "svn" ]; then
+    echo svn co https:plugins.svn/wordpress.org/franklin svn
+    svn co https:plugins.svn/wordpress.org/franklin svn
+fi
+
+
 echo cd svn
+cd svn
+
 echo svn update
+svn update
+
 echo svn add
-echo svn ci -m ‘your commit messages here’
+svn add
+
+echo svn ci -m "$message"
+svn ci -m "$message"
